629d059fb3caedb0d67199f70c8781c32bba4c61
phonescratched <phonescratched@gmail.com>
2019-08-05 23:37:22 -0700

    test commit

---
+++ hello.py
@@ -0,0 +1,19 @@
+import sys
+
+import requests
+
+print(sys.version)
+print(sys.executable)
+print("hello world")
+r = requests.get("https://google.com")
+print(r.status_code)
+x = 12 * 3
+print("ada")
+
+
+def VarHel():
+    print("asdad")
+
+
+print("asdad")
+
